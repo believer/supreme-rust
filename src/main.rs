acf6002ad575a322fc5baa6e4a577ea0968dc77a
Rickard Laurin <rickard.laurin@gmail.com>
2019-06-26 11:56:20 +0200

    feat: create reason frontend

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
 #[macro_use]
 extern crate clap;
+extern crate console;
 
 use clap::{App, Arg};
+use console::style;
 mod reason;
 
 arg_enum! {
@@ -18,18 +20,24 @@ fn main() {
     .author(crate_authors!())
     .arg(
       Arg::with_name("mode")
-        .short("m")
-        .long("mode")
+        .index(1)
         .takes_value(true)
         .possible_values(&Languages::variants())
         .case_insensitive(true)
         .required(true),
     )
+    .arg(Arg::with_name("projectName").index(2).required(true))
     .get_matches();
 
-  let m = value_t!(matches, "mode", Languages).unwrap_or_else(|e| e.exit());
+  println!(
+    "    {} is boostrapping your app",
+    style("Supreme").white().on_red().italic()
+  );
 
-  match m {
-    Languages::Reason => reason::handle(),
+  let mode = value_t!(matches, "mode", Languages).unwrap_or_else(|e| e.exit());
+  let project_name = matches.value_of("projectName").unwrap();
+
+  match mode {
+    Languages::Reason => reason::handle(project_name),
   }
 }
--- src/reason.rs
+++ src/reason.rs
@@ -1,23 +1,52 @@
+extern crate console;
+
+use console::style;
+use indicatif::{ProgressBar, ProgressStyle};
 use std::process::Command;
 
-pub fn handle() {
-  println!("Reaaason");
+pub fn handle(project_name: &str) {
+  let bar = ProgressBar::new(3);
 
-  let output = if cfg!(target_os = "windows") {
-    Command::new("cmd")
-      .args(&["/C", "echo hello"])
-      .output()
-      .expect("failed execute")
-  } else {
-    Command::new("sh")
-      .arg("-c")
-      .arg("echo hello")
-      .output()
-      .expect("failed")
-  };
+  bar.set_style(
+    ProgressStyle::default_bar()
+      .template("\n   [{bar:40.red/white}]")
+      .progress_chars("#>-"),
+  );
+
+  let output = Command::new("sh")
+    .arg("-c")
+    .arg(format!("bsb -init {} -theme react-hooks", project_name))
+    .output()
+    .expect("failed");
+
+  bar.inc(1);
+
+  Command::new("cd")
+    .arg(project_name)
+    .output()
+    .expect("Could not cd in to app");
+
+  bar.inc(1);
+
+  Command::new("npm")
+    .arg("install")
+    .output()
+    .expect("Could not install dependencies");
+
+  bar.finish_and_clear();
 
   if output.status.success() {
-    let s = String::from_utf8_lossy(&output.stdout);
-    println!("nice {}", s);
+    println!(
+      "
+    {} app created!
+
+    - cd {}
+    - npm install
+    - npm start
+    - npm run server (in another terminal window)
+    ",
+      style(project_name).blue(),
+      project_name,
+    );
   }
 }
